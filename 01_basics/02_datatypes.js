1d2aa98ea872da5328d90dd83b8d047947b3fa8e
Priyanka-191925 <71381022+Priyanka-188@users.noreply.github.com>
2023-09-26 00:25:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" ; // treat all js code as newer version.
+
+// alert("Hello");  // we are using node not browser.
+
+console.log(3+
+    3);  // code readability t=should be high
+
+console.log("Priyanka");
+
+
+// datatypes 
+let name= "Priya";
+let age= 22;
+let isLogedIn = "false";
+
+//Types of datatypes
+/* 
+1. number - 2 to power 53
+2. bigint - used for very big numbers.
+3. string = ""
+4. boolean = true/false;
+5. null - standalone value
+6. undefined - no value assigned
+7. symbol - unique
+8. object
+*/
+
+console.log(typeof null);  //null is object 
+console.log(typeof undefined);  // undefined is a type.
