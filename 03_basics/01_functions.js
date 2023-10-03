544fc8d558af43f97f70ce71ca0f4b5343c04ce2
Priyanka-191925 <71381022+Priyanka-188@users.noreply.github.com>
2023-10-03 00:37:13 +0000

    function with object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,12 +9,14 @@ function printMyName(){
     console.log("A");
 }
 
-//calling the function
+// calling the function
+
 // printMyName   ---> print nothing, it represents only the reference of function printMyName
-// printMyName();   //---->executing the function
+printMyName();   //---->executing the function
 
 
-//funtion to add 2 numbers
+
+//  ********* function to add 2 numbers *********
 function add(num1, num2){
     return num1+num2;
 }
@@ -36,6 +38,62 @@ function loginUserName(name="Preet"){
 // loginUserName();            //Welcome undefined
 
 
-// console.log(loginUserName("Kanha"));   //Welcome Kanha
-// console.log(loginUserName(""));         //Please enter a name
+console.log(loginUserName("Kanha"));   //Welcome Kanha
+console.log(loginUserName(""));         //Please enter a name
 console.log(loginUserName());     //Welcome Preet
+
+
+
+
+//****************** */ Functions with object and array  in javascript **************
+
+// (... )  known as rest operator here , it us also called spread , but depends on usecase
+function calculateCartPrice(val1, val2,...num){
+ console.log(val1);           //4
+ return num;                  //
+}
+
+// console.log(calculateCartPrice(4,5,8,23,552,0));    //[ 8, 23, 552, 0 ]  ---->printing array in output
+
+
+
+// **************passing object as argument ******************8
+
+function handleObject(anyobject){
+    console.log(`${anyobject.username} has ${anyobject.id} id.`);
+}
+
+const user = {
+    username : "Priyanka",
+    id : "33r4"
+}
+
+handleObject(user);      //Priyanka has 33r4 id.
+
+const user1 = {
+    name : "Kesav",
+    id : 6464
+}
+
+handleObject(user1);   //undefined has 6464 id.       --->undefined beacuse  username doesnot match with name
+//we can direct pass objects as arguments
+handleObject({
+    username:"Madhav",
+    id : 4533
+})                //Madhav has 4533 id.
+
+
+
+
+// ******************* passing objarray as argument **********************88
+function returnSecondElement(anyArray){
+    return anyArray[1];
+}
+
+const arr = [1,47,36,0,12];       //47
+
+console.log(returnSecondElement(arr));
+
+//we can direct pass an array
+
+console.log(returnSecondElement([2,3,4,5,6]));    //3
