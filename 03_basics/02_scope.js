37b0b3802c149fcc88022bebcbf4a8c3c47cb85b
Priyanka-191925 <71381022+Priyanka-188@users.noreply.github.com>
2023-10-03 11:20:27 +0000

    Local and global scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -31,7 +31,69 @@ var c = 30;
 
 
  {} is scope, curly braces are scope itself.
- 
+
  The value/variable of global scope is available inside local scope, but the value og local scope shold not be 
  available outside the local scope, and this rule is violated by var , therefore we don't recommed to use it
  */
+
+
+ //************* Nested Scopes **********8
+
+ function one(){
+       const username = "Priyanka";
+
+       function two(){
+            const designation = "Learner";
+            // console.log(username + designation);   //PriyankaLearner
+       }
+
+    //    console.log(designation);  //will throw an error because it's scope is local and we are trying to access it outside of its scope
+       
+       two();
+ }
+
+//  console.log(username);    //will throw error because its scope is function one only and we are accessing it outside of it
+
+//  one();
+
+
+
+
+ ////////////////#################////////////////////////
+
+ if(true){
+    const username ="Hitesh";
+
+    if(username === "Hitesh"){
+        const website = "Youtube";
+        // console.log(username + website);    //HiteshYoutube     ---> //accessing username which is its patent scope variable , so will not through error
+    }
+
+    // console.log(website);      //website is not defined --> // throw error accesing local scope variable in its parent     
+}
+
+// console.log(username);    //username is not defined --->  //will throw error as it is local scope varialbe 
+
+
+
+//********************* Interesting *********************8 */
+
+
+console.log(addone(5));     //6 --->give no error  //calling function before declaration
+
+//It is called normal function
+function addone (num){
+    return num + 1;
+}
+
+addone(2);       // return the value  -->3
+
+
+//calling below expression before declaration
+// console.log();(addTwo(5));      // will throw an error ------>  Cannot access 'addTwo' before initialization
+
+//It is also normal function but sometimes it is known as expression.
+const addTwo = function(num){
+    return num +2;
+} 
+addTwo(5);      // return the value  -->7
