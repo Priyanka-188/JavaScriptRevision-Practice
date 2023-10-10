fac86717e562946799ad45a931a0e35ee72d2251
Priyanka-191925 <71381022+Priyanka-188@users.noreply.github.com>
2023-10-10 11:02:37 +0000

    truthy , falsy and Null Coalescing operator

--- 04_control_flow/truthy_and_falsy.js
+++ 04_control_flow/truthy_and_falsy.js
@@ -43,6 +43,11 @@ if(Object.keys(emptyObj).length ==0){
 
 ///Nullish Coalescing Operator (??)  : here we have to see on two values only ---> null, undefined
 
+/**
+ * The nullish coalescing ( ?? ) operator is a logical operator that returns its right-hand side operand
+   when its left-hand side operand is null or undefined , and otherwise returns its left-hand side operand.
+ */
+
 let val1;
 // val1 = 5 ?? 10      //5
 
@@ -58,6 +63,12 @@ console.log(val1);
 
 
 //Terniary operator
+/**
+ * “Ternary” means “composed of three items.”
+ * The conditional (ternary) operator is the only JavaScript operator that takes three operands:
+    a condition followed by a question mark ( ? ), then an expression to execute if the condition is truthy followed by
+    a colon ( : ), and finally the expression to execute if the condition is falsy.
+ */
 
 // syntax:     
 // condition ? true ? false
