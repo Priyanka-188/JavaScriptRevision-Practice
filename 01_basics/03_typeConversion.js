8e6f1d31e060537330a0cfbcd43a1e8f03fdde50
Priyanka-191925 <71381022+Priyanka-188@users.noreply.github.com>
2023-10-10 08:58:45 +0000

    type conversion and operations

--- 01_basics/03_typeConversion.js
+++ 01_basics/03_typeConversion.js
@@ -1,40 +1,40 @@
 let score = "Kahna";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);    //string
+// console.log(typeof(score));    //string
 
-let valInNumber = Number(score);
-console.log(valInNumber);
-console.log(typeof valInNumber);
+// let valInNumber = Number(score);
+// console.log(valInNumber);         // NaN
+// console.log(typeof valInNumber);   //number
 
 /*
 In above case the conversion will work like-
 "33" => 33
 "33abc" => NaN (not a number)
-true =1 ' false=> 0;
+true =>1 ' false=> 0;
 
 */
 
-let isLoggedIn = "Priyanka";
+// let isLoggedIn = "Priyanka";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);               //true
+// console.log(typeof booleanIsLoggedIn);         // boolean
 
 
 /*
 In case of boolean the conversion be like-
 1 will be converted to true.
 0 => false
-"something as  string" => true
+"something as string" => true
 "" => false
 
 */
 
-let num = 45;
-let strNum = String(num);
-console.log(strNum);
-console.log(typeof strNum);
+// let num = 45;
+// let strNum = String(num);
+// console.log(strNum);    //45
+// console.log(typeof strNum);   // string
 
 //################## Operations ###########################
 
@@ -42,9 +42,9 @@ let str1 = "Priyanka";
 let str2 = " Saini";
 
 let str3 = str1+str2;
-console.log(str3);
+console.log(str3);    //Priyanka Saini
 
-console.log("1"+2);      // 12
-console.log(1+"2");      // 12
-console.log("1"+2+3);    // 123
-console.log(1+2+"3");    //33
+// console.log("1"+2);      // 12
+// console.log(1+"2");      // 12
+// console.log("1"+2+3);    // 123
+// console.log(1+2+"3");    //33
