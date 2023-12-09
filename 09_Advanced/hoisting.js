4f280b1654bff52420365960cd0b5a77f31ea788
Priyanka-191925 <71381022+Priyanka-188@users.noreply.github.com>
2023-12-09 10:34:41 +0530

    hoisting in Javascript

---
+++ 09_Advanced/hoisting.js
@@ -0,0 +1,30 @@
+hoistedVariable = "Kridha";
+console.log(hoistedVariable);
+var hoistedVariable ;
+
+hoisted();
+
+function hoisted(){
+    console.log("I am a hoisted function");
+}
+
+var hoisted1 = function(){
+    console.log("I am an expression function using function operator ");
+}
+hoisted1();
+
+
+
+//Hoisting in local scope
+// doSomething();
+
+function doSomething(){
+    x=11;
+    console.log(x);
+    var x;
+}
+doSomething();
+
+
+
+//variable declared using var can be initalised before declaration but in case of let ,it throws an error. 
