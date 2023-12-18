8f3fbd4de309d64af3e438db8e80cdf6aff1b45c
Priyanka <71381022+Priyanka-188@users.noreply.github.com>
2023-12-18 12:36:30 +0530

    maximum number in an array

---
+++ 11_Practice/08.js
@@ -0,0 +1,9 @@
+// // write a function to find the maximum number in an array
+
+let arr= [2,4,56,7,2,100,5444,1,0];
+
+function maxElement(arr){
+    return Math.max(...arr);
+}
+
+console.log(maxElement(arr));
